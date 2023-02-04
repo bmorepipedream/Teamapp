53715828e81af1247a10477f3c4832bbbe346e52
bmorepipedream <19939448+bmorepipedream@users.noreply.github.com>
2023-02-04 03:27:13 -0500

    Adding form changes

--- app.py
+++ app.py
@@ -10,3 +10,16 @@ If you have any questions, checkout our [documentation](https://docs.streamlit.i
 forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+
+with st.form("my_form"):
+   st.write("Inside the form")
+   slider_val = st.slider("Form slider")
+   checkbox_val = st.checkbox("Form checkbox")
+
+   # Every form must have a submit button.
+   submitted = st.form_submit_button("Submit")
+   if submitted:
+       st.write("slider", slider_val, "checkbox", checkbox_val)
+
+st.write("Outside the form")
+
