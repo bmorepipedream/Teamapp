d0e489cecc2cfea00aa75275232631ef88eb3b71
bmorepipedream <19939448+bmorepipedream@users.noreply.github.com>
2023-02-04 20:26:25 -0500

    Adding form changes

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import pandas as pd
 import streamlit as st
 import statsmodels.formula.api as smf
 
-
-
-
 """
 # Welcome to Streamlit!
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
