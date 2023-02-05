fed58292bf7437c7d5d8bf01d09bdadd1a5b4b2f
bmorepipedream <19939448+bmorepipedream@users.noreply.github.com>
2023-02-04 20:40:32 -0500

    Adding form changes

--- app.py
+++ app.py
@@ -22,67 +22,37 @@ fitted = model.fit()
 
 
 with st.form("my_form"):
-   st.write("Inside the form")
-   #slider_val = st.slider("Form slider")
-   #slider2_val = st.slider("Form slider2")
-   #checkbox_val = st.checkbox("Form checkbox")
-   #text_val= st.text_input(
-   #   "Placeholder for the other text input widget",
-    #  "This is a placeholder",
-   #   key="placeholder",
-   #)
+   st.write("Provide Responses Below")
    #RACE
    race = st.selectbox(
-      'Race',
-      ('Black', 'Asian', 'White', 'Two or More'))
-   #st.write('You selected:', race)
+      'Race', ('Black', 'Asian', 'White', 'Two or More'))
    #HISPANICE
    hispanic = st.selectbox(
-      'Hispanic',
-      ('Yes', 'No'))
-   #st.write('You selected:', hispanic)
+      'Hispanic', ('Yes', 'No'))
    #GENDER
    gender= st.selectbox(
-      'Gender',
-      ('Email', 'Home phone', 'Mobile phone'))
-   #st.write('You selected:', gender)
+      'Gender', ('Male', 'Female', 'Other'))
    #EDUCATION
    education = st.selectbox(
-      'Education',
-      ('Email', 'Home phone', 'Mobile phone'))
-   #st.write('You selected:', education)
-
+      'Education', ('Some High School', 'High School Diploma', 'Some College', 'Bachelors', 'Graduate'))
    state = st.selectbox(
       'State',
       ('AL', 'AK', 'AZ', 'AR', 'CA', 'CZ', 'CO', 'CT', 'DE', 'DC','FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY','LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE','NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR','PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI','VA', 'WA', 'WV', 'WI', 'WY'))
-   #st.write('You selected:', state)
-
    age_val= st.number_input(
       "Age", min_value= 18,
-      #"Age",
       key="age",)
-
    expericence_val = st.number_input(
-      "Years of experience", min_value= 0,
-      #"Years of xpericence",
+      "Years of experience", min_value=0,
       key="experience",)
-
    years_val = st.number_input(
-      "Years at company", min_value= 0,
-      #"This is a placeholder",
+      "Years at company", min_value=0,
       key="years",)
-
    job_title = st.selectbox(
       'Job Title',
-      ('Software Engineer', 'Product Manager', 'Data Scientist', 'Hardware Engineer',
-      'Product Designer'))
-   #st.write('You selected:', job_title)
-
+      ('Software Engineer', 'Product Manager', 'Data Scientist', 'Hardware Engineer', 'Product Designer'))
    level = st.selectbox(
       'Level',
       ('Intern', 'Entry Level', 'Intermediate Level', 'Senior Level', ' Manager', 'Senior Manager'))
-   #st.write('You selected:', level)
-
    # Every form must have a submit button.
    submitted = st.form_submit_button("Submit")
 
